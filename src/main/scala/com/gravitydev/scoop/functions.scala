e2ba05447ce9b6af3a7e6858571cfd1a81e3ea8c
Alvaro Carrasco <alvaro@gravitydev.com>
2013-07-25 10:06:31 -0400

    Tweaks after merge and new version

--- src/main/scala/com/gravitydev/scoop/functions.scala
+++ src/main/scala/com/gravitydev/scoop/functions.scala
@@ -6,7 +6,7 @@ import query._
 object `package` {
   def count (expr: ast.SqlExpr[_]) = sql[Int]("COUNT(" +~ expr +~ ")")
   def countDistinct (expr: ast.SqlExpr[_]) = sql[Int]("COUNT(DISTINCT " +~ expr +~ ")")
-  def sum (expr: ast.SqlExpr[_]) = sql[int]("SUM(" +~ expr +~ ")")
+  def sum (expr: ast.SqlExpr[_]) = sql[Int]("SUM(" +~ expr +~ ")")
   def avg [T:SqlType](expr: ast.SqlExpr[T]) = sql[T]("AVG(" +~ expr +~ ")")
   def max [T:SqlType](expr: ast.SqlExpr[T]) = sql[T]("MAX(" +~ expr +~ ")")
   def min [T:SqlType](expr: ast.SqlExpr[T]) = sql[T]("MIN(" +~ expr +~ ")")
