bc24a84a0f76b4f0645c3307c79c870f6931b8c2
Alvaro Carrasco <alvaro@gravitydev.com>
2013-11-02 19:25:29 -0400

    Doc tweaks

--- docs/snippets/installation_sbt.scala
+++ docs/snippets/installation_sbt.scala
@@ -1,5 +1,5 @@
 
-resolvers += "gravitydev" at "http://repo.devstack.io/ws/gravitydev/private"
+resolvers += "gravitydev" at "http://repo.devstack.io/ws/gravitydev/public"
 
 libraryDependencies += "com.gravitydev" %% "scoop" % "0.1.22-SNAPSHOT"
 
